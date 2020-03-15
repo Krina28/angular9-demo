a199514c1c0dba8981724f0064f11a63bd23cf9e
Krina Soni <krinasoni28@gmail.com>
2020-03-15 12:41:15 +0530

    learning basic features

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,17 +2,46 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!!';
+  serverName = 'Test';
+  userName = '';
+  serverCreated = false;
+  servers = ['Test server', 'Test server 2'];
+  isDisplayDetails = false;
+  lists = [];
+  totalClicks = 0;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created!! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onResetUser() {
+    this.userName = '';
+  }
+
+  onDisplayDetails() {
+    this.isDisplayDetails = true;
+    this.totalClicks = this.totalClicks + 1;
+    this.lists.push(this.totalClicks)
+  }
 }
